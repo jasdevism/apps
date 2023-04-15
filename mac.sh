597e9a4f186ac33f39ba7227bf64fa91972f1170
jasdevism <jasdevism@users.noreply.github.com>
2023-04-15 16:33:40 -0400

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Pull this for first time Mac terminal setup and after setting up brew
 
-brew install tree taskwarrior-tui tldr yt-dlp ncdu ffmpeg elinks jq bat htop && echo "$(date "+%m-%d-%H%M"), Essential Apps installed" >> ~/Desktop/messages.txt
+brew install tree nmap telnet  tldr yt-dlp ffmpeg elinks jq iftop htop && echo "$(date "+%m-%d-%H%M"), Essential Apps installed" >> ~/Desktop/messages.txt
 say the installation is complete!
+
+# optional
+# taskwarrior-tui ncdu bat fail2ban
