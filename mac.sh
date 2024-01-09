e91731288320030e0113f1fac525a80b6c626b5e
jasdevism <jasdevism@users.noreply.github.com>
2024-01-09 03:06:11 -0500

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -2,11 +2,13 @@
 # Pull this for first time Mac terminal setup and after setting up brew
 
 brew install \
-tree nmap telnet tldr \
-elinks jq iftop htop \
+tree nmap telnet tldr mlocate \
+elinks jq iftop htop fail2ban \
 yt-dlp ffmpeg \ 
 && echo "$(date "+%m-%d-%H%M"), Essential Apps installed" >> ~/Desktop/messages.txt
 say the installation is complete!
 
-# optional
-# taskwarrior-tui ncdu bat fail2ban
+# OPTIONAL
+# docker portainer dockge awscli
+# taskwarrior-tui ncdu bat 
+
